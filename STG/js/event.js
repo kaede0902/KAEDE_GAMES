7942a2a2a467f891bd17bfde39315f64d6cfb6bb
kaede0902 <kaede0920js@gmail.com>
2019-07-15 23:00:30 +0900

    made func judgeKeyNum

--- STG/js/event.js
+++ STG/js/event.js
@@ -1,13 +1,14 @@
-
-
 const keyPress = {
-    right: false,
-    left: false,
+    right: false, left: false,
+    up: false, down: false,
     counter: 0,
 };
 const printKey = (e) => {
     console.log(`Pressed: ${e.keyCode}`);
     console.log(keyPress);
+}
+const judgeKeyNum = (e,num1,num2,num3) => {
+    return (e.keyCode == num1 ||e.keyCode == num2|| e.keyCode == num3)
 }
 console.log(keyPress);
 console.log('Right:(D,L),Left:(A,H)');
@@ -16,21 +17,30 @@ document.addEventListener('keydown',KeyDownHandler,false);
 document.addEventListener('keyup',KeyUpHandler,false);
 
 function KeyDownHandler(e) {
-    if(
-        e.keyCode == 39|| //->
-        e.keyCode == 68|| // D
-        e.keyCode == 76   // L
-    ) { 
+    if(judgeKeyNum(e,39,68,76)) {
         keyPress.right = true;
-        keyPress.counter++;
+        // ->,D,L
     } else if(
-        e.keyCode == 37|| //<-
-        e.keyCode == 65|| // A
-        e.keyCode == 72// H
+        judgeKeyNum(e,37,65,72)
+        // <-,A,H
     ) {
         keyPress.left = true;
-        keyPress.counter++;
+    } else if(
+        e.keyCode == 38|| // ^
+        e.keyCode == 82|| // w
+        e.keyCode == 75   // k
+    ) {
+        keyPress.up = true;
+    } else if(
+        e.keyCode == 40|| // !^
+        e.keyCode == 83|| // s
+        e.keyCode == 74   // j
+    ) {
+        keyPress.down = true;
     }
+
+
+    keyPress.counter++;
     printKey(e);
 }
 function KeyUpHandler(e) {
