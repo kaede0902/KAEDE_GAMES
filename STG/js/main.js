426d69bb8623d7e2685a7a619088f3ee7619bb33
kaede0902 <kaede0920js@gmail.com>
2019-08-03 20:39:52 +0900

    mod main

--- STG/js/main.js
+++ STG/js/main.js
@@ -2,8 +2,8 @@ const loop = () => {
     ctx.clearRect(0,0,endX,endY);
     ring(player);
     ring(enemy);
-    moveByKeyPress(keyPress,player);
     requestAnimationFrame(loop);
+    moveByKeyPress(keyPress.player);
 }
 loop();
 
